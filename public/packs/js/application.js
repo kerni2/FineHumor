843b564e35e9ed26774cb3a292f341ca6713b264
kerni2 <kerni2@ya.ru>
2023-10-03 20:53:05 +0300

    add some style to devise views

---
+++ public/packs/js/application.js
@@ -0,0 +1,33 @@
+"use strict";
+(self["webpackChunkapp"] = self["webpackChunkapp"] || []).push([["application"],{
+
+/***/ "./app/javascript/application.js":
+/*!***************************************!*\
+  !*** ./app/javascript/application.js ***!
+  \***************************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+__webpack_require__.r(__webpack_exports__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '@rails/ujs'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'turbolinks'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '@popperjs/core'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'bootstrap/js/dist/dropdown'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+// Entry point for the build script in your package.json
+//import * as bootstrap from "bootstrap"
+
+
+
+
+
+Object(function webpackMissingModule() { var e = new Error("Cannot find module '@rails/ujs'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())();
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'turbolinks'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())();
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ var __webpack_exec__ = function(moduleId) { return __webpack_require__(__webpack_require__.s = moduleId); }
+/******/ var __webpack_exports__ = (__webpack_exec__("./app/javascript/application.js"));
+/******/ }
+]);
+//# sourceMappingURL=application.js.map
